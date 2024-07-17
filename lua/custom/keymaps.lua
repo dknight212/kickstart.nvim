790addcea9312fa660a85279be4addefaae029d1
dknight212 <david@dwknight.co.uk>
2024-07-17 18:09:54 +0100

    Add leader vl and vm for Lazy and Mason settings

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -8,6 +8,10 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Settings
+vim.keymap.set('n', '<leader>vl', ':Lazy<CR>', { desc = 'Open Lazy dialog' })
+vim.keymap.set('n', '<leader>vm', ':Mason<CR>', { desc = 'Open Lazy dialog' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
