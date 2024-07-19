b1f26b243ddbca618bdad2340b41bd089cadce40
dknight212 <david@dwknight.co.uk>
2024-07-19 17:59:22 +0100

    Auto-session - change key for save session

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -6,11 +6,18 @@ return {
     auto_session.setup {
       auto_restore_enabled = false,
       auto_session_suppress_dirs = { '~/', '~/Downloads', '~/Documents' },
+      session_lens = {
+        -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+        buftypes_to_ignore = {}, -- list of buffer types that should not be deleted from current session when a new one is loaded
+      },
     }
 
     local keymap = vim.keymap
 
     keymap.set('n', '<leader>wr', '<cmd>SessionRestore<CR>', { desc = 'Restore session for cwd' }) -- restore last workspace session for current directory
-    keymap.set('n', '<leader>ws', '<cmd>SessionSave<CR>', { desc = 'Save session for auto session root dir' }) -- save workspace session for current working directory
+    keymap.set('n', '<leader>wx', '<cmd>SessionSave<CR>', { desc = 'Save session for auto session root dir' }) -- save workspace session for current working directory
   end,
 }
