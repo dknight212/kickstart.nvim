39a9f71d97795c2e28e95b57b08898f445d1479c
dknight212 <david@dwknight.co.uk>
2024-07-21 08:28:04 +0100

    Add bookmarks and to telescope

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -93,7 +93,8 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader>sm', require('auto-session.session-lens').search_session, { desc = '[S]earch Session [Management]' })
+    vim.keymap.set('n', '<leader>sx', require('auto-session.session-lens').search_session, { desc = '[S]earch Session [Management]' })
+    vim.keymap.set('n', '<leader>sm', require('telescope').extensions.bookmarks.list, { desc = '[S]earch Book[m]arks' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
     -- Slightly advanced example of overriding default behavior and theme
