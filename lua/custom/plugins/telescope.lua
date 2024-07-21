f24c365953f9d64ca5c5cd40ee5bbffad3d8243a
dknight212 <david@dwknight.co.uk>
2024-07-21 16:46:39 +0100

    Tidy keymaps and descriptions

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -93,9 +93,10 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader>sx', require('auto-session.session-lens').search_session, { desc = '[S]earch Session [Management]' })
+    vim.keymap.set('n', '<leader>sx', require('auto-session.session-lens').search_session, { desc = '[S]earch Session Management' })
+    vim.keymap.set('n', '<leader>sy', require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc = '[S]earch Workspace Symbols' })
     vim.keymap.set('n', '<leader>sm', require('telescope').extensions.bookmarks.list, { desc = '[S]earch Book[m]arks' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    -- vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
